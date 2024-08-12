ece9f7a90874eb18b46ab31de7d4d4175053a8bb
Xolane3 <143074915+Xolane3@users.noreply.github.com>
2024-08-12 12:09:21 +0200

    Update2 scripts.js

--- scripts.js
+++ scripts.js
@@ -11,16 +11,4 @@ document.addEventListener("DOMContentLoaded", function () {
     toggleContent.classList.toggle("show");
   });
 
-  let bg = document.body;
-  let colorIndex = 0;
-  const colors = ["#1e1e1e", "#333", "#555", "#777", "#999", "#bbb"];
-
-  function animateBackground() {
-    colorIndex = (colorIndex + 1) % colors.length;
-    bg.style.background = `linear-gradient(180deg, ${colors[colorIndex]}, ${
-      colors[(colorIndex + 1) % colors.length]
-    })`;
-  }
-
-  setInterval(animateBackground, 5000);
 });
